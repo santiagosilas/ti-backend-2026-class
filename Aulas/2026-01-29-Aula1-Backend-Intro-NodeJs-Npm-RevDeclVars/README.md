2c2b0f92f33806a4852e6da186e3c4d646fc1616
Silas Santiago <santiagosilas@outlook.com>
2026-01-29 11:01:28 -0300

    Intro Node, Npm, Decl Vars

---
+++ Aulas/2026-01-29-Aula1-Backend-Intro-NodeJs-Npm-RevDeclVars/README.md
@@ -0,0 +1,13 @@
+# Comandos
+
+Para executar via node:
+
+```shell
+node src/main.js
+```
+
+Comando (via package.json):
+
+```shell
+npm run dev
+```
---
+++ Aulas/2026-01-29-Aula1-Backend-Intro-NodeJs-Npm-RevDeclVars/src/main.js
@@ -0,0 +1,19 @@
+console.log("Programa Principal");
+
+// Declaração de variáveis
+const nome = "José da Silva";
+const idade = 42;
+const altura = 1.83;
+const matriculado = false;
+
+let contador = 0;
+contador = contador + 1;
+
+const repr = `N:${nome} I:${idade} A:${altura}`;
+console.log(repr);
+
+// Objetos literais
+const pessoa = {
+  nome: "Lopes Silva",
+  idade: 35,
+};
