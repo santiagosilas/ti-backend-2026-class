f90ae265e99d8346a40b9ff656973573b4f18c49
Silas Santiago <santiagosilas@outlook.com>
2026-01-30 15:46:34 -0300

    Review Javascript

--- Aulas/2026-01-30/src/review.js
+++ Aulas/2026-01-30/src/review.js
@@ -15,3 +15,64 @@ const produto = {
 };
 console.log(`Nome: ${produto.nome}`);
 console.log(`Tipo: ${produto.caracteristicas.tipo}`);
+
+const listaCompras = [
+  "Arroz",
+  {nome: "Feijão", quantidade: 2, unidade: "kg"},
+  {id: 3, nome: "Macarrão", quantidade: 1},
+];
+
+const listaProdutos = [
+  {id: 3, nome: "Teclado", valor: 125.45},
+  {id: 5, nome: "Mouse", valor: 25.45},
+  {id: 7, nome: "GPU Nvidia RTX", valor: 2125.45},
+  {id: 8, nome: "Monitor", valor: 225.45},
+  {id: 9, nome: "Gabinete", valor: 525.45},
+];
+
+console.log(listaProdutos.length);
+for (let i = 0; i < listaProdutos.length; i++) {
+  const nome = listaProdutos[i].nome;
+  const valor = listaProdutos[i].valor;
+  console.log(`${nome.toUpperCase()} ${valor}`);
+}
+for (const produto of listaProdutos) {
+  const nome = produto.nome;
+  const valor = produto.valor;
+  console.log(`${nome.toUpperCase()} ${valor}`);
+}
+
+function printf(mensagem) {
+  const msg = mensagem.toUpperCase();
+  console.log(`[LOG] ${msg}`);
+  console.log("[LOG] Fim da mensagem");
+}
+
+printf("Hello, World!");
+
+function fabricarProduto(id, nome, valor) {
+  const produto = {
+    id: id,
+    nome: nome,
+    preco: valor,
+  };
+  return produto;
+}
+
+function fazerBolo(sabor, tipo) {
+  const novoBolo = {
+    sabor: sabor,
+    tipo: tipo,
+  };
+  return novoBolo;
+}
+
+const bolo1 = fazerBolo("Nata/Goiaba", "Fofo");
+const bolo2 = fazerBolo("Chocolate", "Fofo");
+const bolo3 = fazerBolo("Limão", "Fofo");
+console.log(bolo1.tipo);
+console.log(bolo1.sabor);
+console.log(bolo2.tipo);
+console.log(bolo2.sabor);
+console.log(bolo3.tipo);
+console.log(bolo3.sabor);
